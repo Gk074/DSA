9a412c416d8f884c851136d5ddab24c7329bc391
Gk074 <95033295+Gk074@users.noreply.github.com>
2026-03-10 00:46:57 -0400

    Time: 0 ms (100.00%) | Memory: 13 MB (79.28%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,11 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        if len(nums) is 0:
-            return 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                current = nums[i]+ nums[j]
-                if current==target:
-                    return[i,j]
+        h={}
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in h:
+                return [h[diff],i]
+            h[n]=i
+        return[]
     
