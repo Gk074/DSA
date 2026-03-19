22f797d7b6e8dcfbf2000e3d98af01c7e3a23be3
Gk074 <95033295+Gk074@users.noreply.github.com>
2026-03-18 21:04:36 -0400

    Time: 0 ms (100%), Space: 12.5 MB (12.87%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -5,5 +5,10 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        import operator
-        return operator.add(a,b)
+        MASK = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
+        while b!=0:
+            c = ((a&b)<<1) & MASK
+            a = (a^b) & MASK
+            b=c
+        return a if a <= MAX_INT else ~(a ^ MASK)
