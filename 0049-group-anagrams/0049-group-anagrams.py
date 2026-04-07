5a1d9ec411c2a1f39fd5c24bf91c5bb6055c7406
Gk074 <95033295+Gk074@users.noreply.github.com>
2026-04-07 00:17:54 -0400

    Time: 33 ms (19.17%), Space: 18.1 MB (20.32%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        if len(strs)==1 or len(strs) == 0:
+            return [strs]
+        ans = defaultdict(list)
+        
+        for i in strs:
+            key = [0]*26
+            for c in i:
+                key[ord(c)-ord('a')]+=1
+            key = tuple(key)
+            ans[key].append(i)
+        return ans.values()
