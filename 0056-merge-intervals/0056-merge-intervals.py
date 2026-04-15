edbc19b7ed7d609266c15fc17656eb9c060df878
Gk074 <95033295+Gk074@users.noreply.github.com>
2026-04-15 17:39:30 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,15 +4,26 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        if not intervals:
-            return []
-        intervals.sort(key = lambda x: x[0])
-        merged = [intervals[0]]
-        for start,end in intervals[1:]:
-            last_end = merged[-1][1]
-            if start<=last_end:
-                merged[-1][1] = max(end, last_end)
+        # if not intervals:
+        #     return []
+        # intervals.sort(key = lambda x: x[0])
+        # merged = [intervals[0]]
+        # for start,end in intervals[1:]:
+        #     last_end = merged[-1][1]
+        #     if start<=last_end:
+        #         merged[-1][1] = max(end, last_end)
+        #     else:
+        #         merged.append([start,end])
+        # return merged
+
+        new=[]
+        intervals.sort()
+        for i in intervals:
+            if not new or i[0]>new[-1][1]:
+                new.append(i)
             else:
-                merged.append([start,end])
-        return merged
+                end = max(i[1], new[-1][1])
+                new[-1][1] = end
+        return new
+
         
