8a53b6e15a9cf2213c309851e08893862afe9a6e
Gk074 <95033295+Gk074@users.noreply.github.com>
2026-04-15 17:18:57 -0400

    Time: 14 ms (23.28%), Space: 17.1 MB (11.54%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not intervals:
+            return []
+        intervals.sort(key = lambda x: x[0])
+        merged = [intervals[0]]
+        for start,end in intervals[1:]:
+            last_end = merged[-1][1]
+            if start<=last_end:
+                merged[-1][1] = max(end, last_end)
+            else:
+                merged.append([start,end])
+        return merged
+        
