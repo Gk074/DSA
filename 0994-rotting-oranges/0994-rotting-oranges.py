87cb6cbf1036ceddcb68a7fe089584a99f536892
Gk074 <95033295+Gk074@users.noreply.github.com>
2026-04-16 14:10:36 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -12,18 +12,16 @@ class Solution(object):
         for i in range(row):
             for j in range(column):
                 if grid[i][j]==2:
-                    q.append((i,j))
+                    q.append((i,j,0))
                 if grid[i][j]==1:
                     fresh+=1
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
         while q and fresh>0:
-            for i in range(len(q)):
-                r,c = q.popleft()
-                for dr,dc in directions:
-                    nr,nc = r+dr, c+dc
-                    if 0<=nr<row and 0<=nc<column and grid[nr][nc]==1:
-                        grid[nr][nc]=2
-                        fresh-=1
-                        q.append((nr,nc))
-            minutes+=1
+            r,c = q.popleft()
+            for dr,dc in directions:
+                nr,nc = r+dr, c+dc
+                if 0<=nr<row and 0<=nc<column and grid[nr][nc]==1:
+                    grid[nr][nc]=2
+                    fresh-=1
+                    q.append((nr,nc, minutes+=1))
         return minutes if fresh==0 else -1
